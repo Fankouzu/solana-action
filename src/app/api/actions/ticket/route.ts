e8f0cdcce4966bac2bea1c9bedd2b3aa2dbd69c7
崔棉大师 <1046166@qq.com>
2024-08-28 10:58:41 +0800

    update event 0829

--- src/app/api/actions/ticket/route.ts
+++ src/app/api/actions/ticket/route.ts
@@ -30,7 +30,7 @@ export const GET = (req: Request) => {
     const payload: ActionGetResponse = {
       icon: new URL("/event_20240829.jpg", new URL(req.url).origin).toString(),
       title: "Blinks玩起来 丨03丨Blinks如何更好的赋能项目？",
-      disabled:true,
+      disabled:false,
       description:
         `8月29日20:00（UTC+8），聚集最早一批尝鲜Blinks应用场景的项目方，从项目的角度去理解：Blinks如何更好的赋能项目？
 付费公开课：0.05 SOL`,
@@ -44,7 +44,7 @@ export const GET = (req: Request) => {
               {
                 name: "email",
                 label: "请输入接收门票的邮箱",
-                required: false,
+                required: true,
               },
             ],
           },
